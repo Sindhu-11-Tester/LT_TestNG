3be9deaa808a9adb80328f4ffad0baa7ea1bc25b
ShindeSindhutai <sindhutaishinde11@gmail.com>
2025-01-13 21:57:34 +0530

    Tstng
    TestNGLamdatest

---
+++ src/main/java/LT_Scenario_TNG/LT_TestNG/App.java
@@ -0,0 +1,13 @@
+package LT_Scenario_TNG.LT_TestNG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
